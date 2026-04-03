976ba1dcf2b57b27aeb6bcf4ab125ef4f4c4b985
Valerie Hirsch <Valerie.Hirsch@stud.hcw.ac.at>
2026-04-03 08:42:31 +0200

    init, implemented erxercise 1

--- server/files/index.js
+++ server/files/index.js
@@ -8,7 +8,64 @@ window.onload = function () {
         /* Task 1.3. Add your code from exercise 1 here 
            and include a non-functional 'Edit' button
            to pass this test */
-      }
+     
+          fetch('/movies')
+          .then(response1 => response1.json())
+          .then(movies => {
+              const container = document.getElementById('movies-container');
+              movies.forEach(movie => {
+              const movieDiv = document.createElement('div');
+              movieDiv.className = 'movie';
+
+              const poster = document.createElement('img');
+              poster.src = movie.Poster;
+              poster.alt = movie.Title;
+              movieDiv.appendChild(poster);
+
+              //Ratings
+              let imdbClass = 'good';
+              if (movie.imdbRating <= 7) {
+                  imdbClass = 'medium';
+              } else if (movie.imdbRating <= 5) {
+                  imdbClass = 'bad';
+              }
+
+              let metaClass = 'good';
+              if (movie.Metascore <= 70) {
+                  metaClass = 'medium';
+              } else if (movie.Metascore <= 50) {
+                  metaClass = 'bad';
+              }
+
+              const imdbHTML = `<span class="rating ${imdbClass}">IMDB Rating: ${movie.imdbRating}</span>`;
+              const metaHTML = `<span class="rating ${metaClass}">Metascore: ${movie.Metascore}</span>`;
+
+              const infoDiv = document.createElement('div');
+              infoDiv.innerHTML = `
+                  <h2>${movie.Title} (${movie.Released})</h2>
+                  <p>Runtime: ${movie.Runtime} min</p>
+                  <p>Genres: ${movie.Genres.join(', ')}</p>
+                  <p>Directors: ${movie.Directors.join(', ')}</p>
+                  <p>Writers: ${movie.Writers.join(', ')}</p>
+                  <p>Actors: ${movie.Actors.join(', ')}</p>
+                  <p> ${imdbHTML}</p>
+                  <p>${metaHTML}</p>
+                  <p>Plot: ${movie.Plot}</p>
+              `;
+              movieDiv.appendChild(infoDiv);
+
+              
+              container.appendChild(movieDiv);
+
+
+        });
+    })
+            
+
+
+
+          }
+
 
     } else {
       bodyElement.append(
--- server/server.js
+++ server/server.js
@@ -13,9 +13,51 @@ app.use(express.static(path.join(__dirname, 'files')));
 
 // Configure a 'get' endpoint for all movies..
 app.get('/movies', function (req, res) {
-  /* Task 1.2. Remove the line below and eturn the movies from 
-     the model as an array */
-  res.sendStatus(404)
+ fetch("http://www.omdbapi.com/?apikey=a8cbe32d&s=Bridget+Jones")
+      .then(response => response.json())
+      .then(data => {
+      const twoMovies = data.Search.slice(0,2)
+
+    fetch("http://www.omdbapi.com/?apikey=a8cbe32d&s=mamma+mia")
+      .then(response => response.json())
+      .then(data => {
+      const twoMovies2 = data.Search.slice(0,2)  
+      
+    fetch("http://www.omdbapi.com/?apikey=a8cbe32d&s=love+actually")
+      .then(response => response.json())
+      .then(data => {
+      const oneMovie = data.Search.slice(0,1)
+
+      const mergedMovies = twoMovies.concat(oneMovie);
+      const threeMovies = mergedMovies.concat(twoMovies2);
+
+      Promise.all(
+       threeMovies.map(movie => 
+        fetch(`http://www.omdbapi.com/?apikey=a8cbe32d&i=${movie.imdbID}`)
+        .then(response2 => response2.json())
+      )
+    ).then(fullMovies => {
+
+        const structured = fullMovies.map(movie => ({
+          Title: movie.Title,
+          Released: new Date(movie.Released).toLocaleDateString('en-CA'),
+          Runtime: parseInt(movie.Runtime),
+          Genres: (movie.Genre).split(','),
+          Directors: (movie.Director).split(','),
+          Writers: (movie.Writer).split(','),
+          Actors: (movie.Actors).split(','),
+          Plot: movie.Plot,
+          Poster: movie.Poster,
+          Metascore: parseInt(movie.Metascore),
+          imdbRating: parseFloat(movie.imdbRating)
+        }))
+        res.json(structured)
+        })
+      })
+    })
+  })
+
+  //res.sendStatus(404)
 })
 
 // Configure a 'get' endpoint for a specific movie
